f797b2e9933043d65a45adeffafcd59e182a5277
Kotlaev Danil <kotlaevdanil@gmail.com>
2022-03-30 13:04:33 +0400

    reviews tabs

---
+++ assets/js/tabs.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    const findBlockByAlias = (alias) => {
+        return $(".reviews__item").filter((ndx, item) => {
+            return $(item).attr("data-linked-with") === alias;
+        });
+    }
+    $(".interactive-avatar__link").on("click", (e) => {
+        e.preventDefault();
+        const $this = $(e.currentTarget);
+        const target = $this.attr("data-open");
+        const itemToShow = findBlockByAlias(target);
+        const curItem = $this.closest(".reviews__switcher-item");
+        itemToShow.addClass("active").siblings().removeClass("active");
+        curItem.addClass("active").siblings().removeClass("active");
+    });
+});
